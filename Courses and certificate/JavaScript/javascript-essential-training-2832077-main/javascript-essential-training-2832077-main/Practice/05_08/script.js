d3219826878b55e4926c4eace9cdbe36655aad96
Emmanuel Ngongo <129612476+EmmanuelNgongo@users.noreply.github.com>
2023-05-25 00:00:19 +0200

    Update

--- Courses and certificate/JavaScript/javascript-essential-training-2832077-main/javascript-essential-training-2832077-main/Practice/05_08/script.js
+++ Courses and certificate/JavaScript/javascript-essential-training-2832077-main/javascript-essential-training-2832077-main/Practice/05_08/script.js
@@ -2,4 +2,12 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
+ * Find an element and add two different classes
+ * Remove only one of the new classes
+ * Add a new attribute to an element  // data_attribute
+ * Request the value of the attribute
+ * Change the value of the attribute
+ * Add some inline CSS to an element by specifying the style property and setting its value
+ * Query the style property of an element in the console to get a list of all the style
+ * properties available
  */
