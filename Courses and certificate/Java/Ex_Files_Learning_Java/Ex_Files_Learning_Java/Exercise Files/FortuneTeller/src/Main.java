f5f47c8369f09e28ae60f6ee8eac3bff3941e180
Emmanuel Ngongo <129612476+EmmanuelNgongo@users.noreply.github.com>
2023-06-25 15:25:00 +0200

    Java Update

--- Courses and certificate/Java/Ex_Files_Learning_Java/Ex_Files_Learning_Java/Exercise Files/FortuneTeller/src/Main.java
+++ Courses and certificate/Java/Ex_Files_Learning_Java/Ex_Files_Learning_Java/Exercise Files/FortuneTeller/src/Main.java
@@ -1,18 +1,20 @@
+import com.sun.source.tree.WhileLoopTree;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //boolean run = true;
         System.out.println("Pick a number between 1 to 10");
+        //System.out.println("Choose -1 to close the program");
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
-
-        if (num >= 0 && num <= 5) {
-            System.out.println("Enjoy the good luck of money and health coming to you");
-        } else if (num >= 5 && num <= 10) {
-            System.out.println("Enjoy the good luck of happiness and satisfaction coming to you");
-        } else {
-            System.out.println("Invalid Input, Choose a number between 1 and 10");
+            if (num >= 0 && num <= 5) {
+                System.out.println("Enjoy the good luck of money and health coming to you");
+            } else if (num >= 5 && num <= 10) {
+                System.out.println("Enjoy the good luck of happiness and satisfaction coming to you");
+            } else {
+                System.out.println("Invalid Input, Choose a number between 1 and 10");
+            }
         }
-
     }
-}
