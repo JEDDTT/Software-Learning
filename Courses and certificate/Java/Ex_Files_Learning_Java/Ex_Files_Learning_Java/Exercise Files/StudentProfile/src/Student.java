2346676738e7655b05e0e0413485b5e7f5ae6cec
Emmanuel Ngongo <129612476+EmmanuelNgongo@users.noreply.github.com>
2023-06-29 08:17:34 +0200

    Update

--- Courses and certificate/Java/Ex_Files_Learning_Java/Ex_Files_Learning_Java/Exercise Files/StudentProfile/src/Student.java
+++ Courses and certificate/Java/Ex_Files_Learning_Java/Ex_Files_Learning_Java/Exercise Files/StudentProfile/src/Student.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    String firstName;
+    String lastName;
+    int expectedGraduationYear;
+    double gpa;
+    String declaredMajor;
+
+    public Student (String firstName, String lastName,
+                    int expectedGraduationYear, double gpa,
+                    String declaredMajor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.expectedGraduationYear = expectedGraduationYear;
+        this.gpa = gpa;
+        this.declaredMajor = declaredMajor;
+    }
+
+    public void incrementExpectedGraduationYear () {
+        this.expectedGraduationYear = this.expectedGraduationYear + 1;
+
+    }
 }
