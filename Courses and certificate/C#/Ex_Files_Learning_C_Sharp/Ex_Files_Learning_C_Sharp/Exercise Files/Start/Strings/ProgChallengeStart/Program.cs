82a086c93ae4dce05a43ff31b04792825adca362
Emmanuel Ngongo <129612476+EmmanuelNgongo@users.noreply.github.com>
2023-07-11 20:03:09 +0200

    C# Challenge Update

--- Courses and certificate/C#/Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Strings/ProgChallengeStart/Program.cs
+++ Courses and certificate/C#/Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Strings/ProgChallengeStart/Program.cs
@@ -12,7 +12,7 @@ namespace ProgChallengeStart
             // Print the game greeting and instructions
             Console.WriteLine("Let's Play 'Guess the Number'!");
             Console.WriteLine("I'm thinking of a number between 0 and 20.");
-            Console.WriteLine("Enter your guess, or -1 to give up.");
+            Console.WriteLine("Enter your guess, or -1 to stop the game.");
 
             // Keep track of the number of guesses and the current user guess
             int guessCount = 0;
@@ -20,13 +20,37 @@ namespace ProgChallengeStart
 
             // Start the game and run until user quits or guesses correctly
             // HINT: You'll need a way to convert the user's input to an integer
-            while (currentGuess != -1)
+            //While loop is not appropriate 
+            //      USING DO LOOP 
+            do
             {
+                Console.WriteLine("What is your guess");
                 String input = Console.ReadLine();
-
-
-
-            }
+                // Converting the input using try parse
+                bool succeeded = false;
+                succeeded = Int32.TryParse(input, out currentGuess);
+                //if the input conversion is successful we compare it with the the random number
+                if (succeeded)
+                {
+                    guessCount++;
+                    if (currentGuess == theNumber)
+                    {
+                        Console.WriteLine($"You got it in {guessCount} guesses");
+                    }
+                    else if (currentGuess >= 0 && currentGuess < theNumber)
+                    {
+                        Console.WriteLine($"Nope, Higher than that");
+                    }
+                    else if (currentGuess > theNumber)
+                    {
+                        Console.WriteLine($"Nope, lower than that");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hum, that doesn't look like a number, try again");
+                }
+            } while (currentGuess != -1);
         }
     }
 }
