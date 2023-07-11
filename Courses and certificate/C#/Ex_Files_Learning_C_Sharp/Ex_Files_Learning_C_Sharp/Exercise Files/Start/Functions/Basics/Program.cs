28152bc38f20a8153dc40d9a458c27af08752162
Emmanuel Ngongo <129612476+EmmanuelNgongo@users.noreply.github.com>
2023-07-11 21:25:51 +0200

    update

--- Courses and certificate/C#/Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Functions/Basics/Program.cs
+++ Courses and certificate/C#/Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Functions/Basics/Program.cs
@@ -2,15 +2,20 @@
 
 // Functions are used to group re-usable code together in a single
 // unit that can be customized with parameters.
-
 // TODO: Functions have a return type, name, and optional parameters
-
-
+float MilesToKm(float miles)
+{
+    float result = miles * 1.6f;
+    return result;
+}
 // TODO: A function with no return value has a 'void' type
-
-
+void PrintWithPrefix(String thestr)
+{
+    Console.WriteLine($"::> {thestr}");
+}
 // TODO: Call first function
-
-
+Console.WriteLine(MilesToKm(4f));
+Console.WriteLine(MilesToKm(8f));
 // TODO: Call second function
-
+PrintWithPrefix("Emmanuel");
+PrintWithPrefix("Ngongo");
